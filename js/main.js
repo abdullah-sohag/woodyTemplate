1580808b3ac0c7fb75154f6d86013dad7d28fb94
abdullah sohag <abdullahsohag74@gmail.com>
2023-08-02 15:47:31 +0600

    testimonial

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 let splide = new Splide( '#baner' );
     splide.mount();
 
+let splide2 = new Splide( '#slid-testimonial', {
+    type   : 'loop',
+    padding: '25%',
+    pagination: false,
+  }  );
+    splide2.mount();
+
 function allService(non = true) {
     let div = document.querySelectorAll('[data-project-type]')
     if (non) {
