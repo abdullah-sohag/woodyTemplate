32840ab9a5362ee4ea2d4fe4275784dd7d7ce5d6
abdullah sohag <abdullahsohag74@gmail.com>
2023-08-02 09:38:51 +0600

    free quote

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 
 let splide = new Splide( '#baner' );
     splide.mount();
+
+function allService(non = true) {
+    let div = document.querySelectorAll('[data-project-type]')
+    if (non) {
+        for (let e of div) {
+            e.classList.add('d-none')
+        }
+    } else {
+        for (let e of div) {
+            e.classList.remove('d-none')
+        } 
+    }
+    
+    
+}
+
+function serviceSort(arg) {
+    allService()
+    let selectDivs = document.querySelectorAll(`[data-project-type='${arg}']`)
+    
+    for(let e of selectDivs){
+        e.classList.remove('d-none')
+    }
+}
